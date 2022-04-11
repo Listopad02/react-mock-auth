638cc62eb6d5c101e0cd3bbdcf4637aa9cde42af
Listopad02 <123ayetvbass@gmail.com>
2022-04-11 16:02:50 +0300

    auth forms component realized

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import './Auth.css'
 
 const Auth = () => {
   return (
-    <div>Auth page</div>
+    <div className="Auth">
+      <div className='Auth-container'>
+        <h1 className='Auth-container__header'>Authorization</h1>
+        <form className="Auth-container__form">
+          <div className='Auth-container__block'>
+            <input type="text" className='Auth-container__input'/>
+            <input type="text" className='Auth-container__input'/>
+          </div>
+          <button className='Auth-container__btn'>Enter</button>
+          <button className='Auth-container__btn'>Register</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
