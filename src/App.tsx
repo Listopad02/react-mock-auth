6555e7fd74278f7b3d000d3145bba8434d5e55c9
Listopad02 <123ayetvbass@gmail.com>
2022-04-11 12:23:36 +0300

    base project have been created (components prototypes and routing files have been created)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC } from 'react';
+import Layout from './hoc/Layout';
+import Auth from './components/Auth/Auth';
+import Contacts from './components/Contacts/Contacts';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
-function App() {
+const App: FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Auth />} />
+          <Route path='/contacts' element={<Contacts />} />
+        </Routes>
+      </Layout>
     </div>
   );
 }
